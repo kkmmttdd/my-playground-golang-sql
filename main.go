8819b570f011d2762c203c06bc2f44be1b54e7d9
kkmmttdd <kkmmttdd@gmail.com>
2020-10-21 18:36:40 +0900

    can read row

--- main.go
+++ main.go
@@ -6,6 +6,12 @@ import (
 	"log"
 )
 
+var (
+	//id int
+	//name string
+	host string
+)
+
 func main() {
 	db, err := sql.Open("mysql",
 		"root@tcp(127.0.0.1:3306)/hello")
@@ -13,6 +19,25 @@ func main() {
 		log.Fatal(err)
 	}
 	err = db.Ping()
+	rows, err := db.Query("select host from mysql.user")
+	//rows, err := db.Query("select id, name from users where id = ?", 1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		//err := rows.Scan(&id, &name)
+		err := rows.Scan(&host)
+		if err != nil {
+			log.Fatal(err)
+		}
+		//log.Println(id, name)
+		log.Println(host)
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
 	if err != nil {
 		// do something here
 		log.Fatal(err)
